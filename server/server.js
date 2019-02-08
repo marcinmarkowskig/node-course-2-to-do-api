283ae33dd547011fc06e6e4e53c6e8878ca6c804
marcinmarkowskig <marcin.markowski.mmww@gmail.com>
2019-02-08 11:24:28 +0100

    Test POST /todos routes

--- server/server.js
+++ server/server.js
@@ -23,6 +23,8 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on 3000.')
 })
+
+module.exports = {app};
 //stworzenie nowej instancji modelu
 // var newTodo = new Todo({
 //   text: 'Cook dinner'
