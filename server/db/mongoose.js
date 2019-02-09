3742ae1fea2db5f4d14d6d2aeed05553522d8c24
marcinmarkowskig <marcin.markowski.mmww@gmail.com>
2019-02-09 12:39:34 +0100

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //połączenie z bazą danych
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp' || "mongodb://MarcinMar:MarcinMar@ds127545.mlab.com:27545/mongo-data-node-app");
 
 module.exports = {
   mongoose
