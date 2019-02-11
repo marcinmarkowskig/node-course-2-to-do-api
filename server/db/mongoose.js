14b7f9406820a104156595a61fe7403beef4e03a
marcinmarkowskig <marcin.markowski.mmww@gmail.com>
2019-02-11 14:00:47 +0100

    Setup seperate test and development envs.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //połączenie z bazą danych
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp' || "mongodb://MarcinMar:MLab44@ds127545.mlab.com:27545/mongo-data-node-app");
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
